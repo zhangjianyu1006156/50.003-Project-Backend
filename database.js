2bf49dd54953b22f19f7c65472f5d5280b2c5b0b
Paul Ong <paulongzs@gmail.com>
2023-08-05 01:37:39 +0800

    Commit
    Adjusted user and booking schemas, added endpoints for booking and included app.use(cors(..)) to allow for cross site requests given that the requesting site is specified first

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+// const axios = require("axios");
+// const express = require("express");
+// const cors = require("cors");
+const { User } = require("./models/user");
+// const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-const uri = process.env.DATABASE_URL;
+// const uri = process.env.DATABASE_URL;
 
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
+// const app = express.Router();
+// app.use(cors());
 
 //makes 16 char user ID
 const makeId = () => {
@@ -17,26 +22,43 @@ const makeId = () => {
 };
 
 //this makes example document, hook it up with signup button
-async function run(collectionname,data) {
-  try {
-    const database = client.db("testdb"); //replace this with db name
-    const user = database.collection(collectionname);
+async function run(data) {
+  const use_id = makeId();
+  // try {
+  // const database = client.db("test"); //replace this with db name
+  // const user = database.collection(collectionname);
 
-    // this also needs a push request to send the token key to this express.js
-    // this needs to be have an axios function to get back the profile data
-    const userdata = {
-      id: makeId(),
-      data,
-      name: "test2item",
-      bookings: [],
-    };
-    user.insertOne(userdata);
+  // this also needs a push request to send the token key to this express.js
+  // this needs to be have an axios function to get back the profile data
+  const userdata = new User({
+    user_id: use_id,
+    data: data,
+    name: "test2item",
+  });
+  await userdata
+    .save()
+    .then((savedUser) => {
+      console.log("userdata added:", savedUser);
+    })
+    .catch((err) => {
+      console.log("Error saving user data:", err);
+    });
+  // user.insertOne(userdata);
 
-    console.log("userdata added:", userdata);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    client.close();
-  }
+  console.log("userdata added:", userdata);
+  // axios
+  //   .post("/profile ", { key: userdata.id })
+  //   .then((res) => {
+  //     console.log("id sent to frontend");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // } finally {
+  //   // Ensuers that the client will close when you finish/error
+  //   client.close();
+  // }
+  return use_id;
 }
 module.exports = { run };
 //run().catch(console.dir);
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 require("dotenv").config();
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    // Handle connection error, possibly exit the application
+  }
+}
+
+connectToDatabase();
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected successfully to database"));
 
 app.use(express.json());
+const allowedOrigins = ["http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 // app.use(function(req, res, next) {
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
