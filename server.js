2d6b1afde8c85e140f1374b554dea41945e23114
nigel <nigelpjk@gmail.com>
2023-08-05 10:36:09 +0000

    Backend Changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const cors_proxy = require("cors-anywhere");
+// const cors_proxy = require("cors-anywhere");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -68,14 +68,14 @@ app.use(function(req, res, next) {
 //         },
 //     })
 // );
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next();
+//   });
 
 const productsRouter = require("./routes/products");
 app.use("/products", productsRouter);
